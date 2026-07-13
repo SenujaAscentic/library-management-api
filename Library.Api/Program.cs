bd41777e40d0502d49f9b4ee136c0e659a7b4a21
Senuja Bodhinayake <senuja.bodhinayake@ascentic.se>
2026-07-13 23:05:12 +0530

    registered repositories in program file

--- Library.Api/Program.cs
+++ Library.Api/Program.cs
@@ -1,4 +1,6 @@
 using Library.Api.Infrastructure.Data;
+using Library.Api.Infrastructure.Repositories.Implementations;
+using Library.Api.Infrastructure.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +12,10 @@ builder.Services.AddDbContext<LibraryDbContext>(options =>
 });
 
 builder.Services.AddOpenApi();
+
+builder.Services.AddScoped<IBookRepository, BookRepository>();
+builder.Services.AddScoped<IBorrowingRepository, BorrowingRepository>();
+builder.Services.AddScoped<IMemberRepository, MemberRepository>();
 
 var app = builder.Build();
 
